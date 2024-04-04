37d09cb90b5de7d19efa67594eb1032f9e61bc5d
Adam Vinch <adamvinch@gmail.com>
2024-04-04 10:11:16 -0400

    Update vector methods to work with non vectors

--- src/vector.js
+++ src/vector.js
@@ -1,26 +1,34 @@
-class Vector2d {
+export class Vector2d {
   constructor(x, y) {
     this.x = x;
     this.y = y;
   }
 
-  add(other) {
-    this.x += other.x;
-    this.y += other.y;
+  add(v) {
+    if (v instanceof Vector2d) return new Vector2d(this.x + v.x, this.y + v.y);
+    return new Vector2d(this.x + v, this.y + v)
   }
 
-  sub(other) {
-    this.x -= other.x; 
-    this.y -= other.y;
+  sub(v) {
+    if (v instanceof Vector2d) return new Vector2d(this.x - v.x, this.y - v.y);
+    return new Vector2d(this.x - v, this.y - v)
+  }
+  multiply(v) {
+    if (v instanceof Vector2d) return new Vector2d(this.x * v.x, this.y * v.y);
+    return new Vector2d(this.x * v, this.y * v)
   }
 
-  dot(other) {
-    return this.x * other.x + this.y * other.y;
+  dot(v) {
+    if (v instanceof Vector2d) return this.x * v.x + this.y * v.y;
+    return this.x * v + this.y * v;
   }
 
   magnitude() {
-    return Math.sqrt(this.reduce((sum, currentVal) => {
-      return sum + currentVal ** 2
-    }, 0))
+    return Math.sqrt(this.x ** 2 + this.y ** 2);
+  }
+
+  norm() {
+    const mag = this.magnitude();
+    return new Vector2d(this.x / mag, this.y / mag)
   }
 }
